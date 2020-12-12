cf5c6fbb0af54b1e0272b5b782eed0b2a8481d33
Eliana <eliana.perez@mymail.champlain.edu>
2020-12-12 12:08:20 -0500

    reorganized some stuff and added different scenes for different triggers

---
+++ DialogueSystem/Assets/Scripts/NPCInteract.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NPCInteract : NPCTrigger
+{
+    public CanvasGroup NPCCanvas;
+    private bool inRange = false;
+
+    private void Update()
+    {
+        if (inRange && Input.GetKeyDown(KeyCode.Q))
+        {
+            TriggerDialogue();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(tagToInitiateDialogue))
+        {
+            NPCCanvas.alpha = 1;
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(tagToInitiateDialogue))
+        {
+            NPCCanvas.alpha = 0;
+            inRange = false;
+        }
+    }
+}
---
+++ DialogueSystem/Assets/ThingsForTesting/MovementScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(moveH,0f,0f) * 2.0f * Time.deltaTime;
+    }
+}
